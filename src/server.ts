d4a7efe6d373b77a57c06f0e1348341f0f52b03c
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-07-24 21:51:35 -0300

    devCommit

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import express from "express";
 
 const app = express();
 
-// configurando o prettier e eslint
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World!" });
 });
