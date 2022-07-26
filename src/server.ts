3d0cd6377e52a13b563cf9222994c153e9574584
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-07-26 13:29:52 -0300

    devCommit

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from "express";
 
 const app = express();
 
+// Utilizamos isso para aceitar o JSON
+app.use(express.json());
+
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World!" });
 });
@@ -11,4 +14,4 @@ app.post("/course", (request, response) => {
   return response.json({ name });
 });
 
-app.listen(3333, () => console.log("Server is running! 🔥🚀"));
+app.listen(3333, () => console.log("Server is running in port 3333! 🔥🚀"));
