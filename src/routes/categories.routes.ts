df859f7b7286a34baa5a5d8948b1e81a8a73e9c9
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-07-28 11:24:08 -0300

    devCommit

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,16 @@
 import { Request, Response, Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  categoriesRepository.create({ name, description });
 
-  categories.push(category);
-
-  return response.status(201).json(categories);
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
