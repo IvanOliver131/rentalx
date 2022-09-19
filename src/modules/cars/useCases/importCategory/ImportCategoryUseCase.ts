f40dffcddcf804add722b40f335b8cbb44e0e467
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-09-19 10:54:20 -0300

    feat: importação de arquivos

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,15 +1,58 @@
+/* eslint-disable array-callback-return */
 import { parse } from "csv-parse";
 import fs from "fs";
 
+import { ICategoriesRepository } from "../../interfaces/ICategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    // Faz a leitura do arquivo em partes
-    const stream = fs.createReadStream(file.path);
-    const parseFile = parse();
-    stream.pipe(parseFile);
-    // commit
-    parseFile.on("data", async (line) => {
-      console.log(line);
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      console.log(file);
+      // Faz a leitura do arquivo em partes
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+      const parseFile = parse();
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          // Retorna ["name", "description"]
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.map((category: IImportCategory) => {
+      const { name, description } = category;
+
+      const existCategory = this.categoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
     });
   }
 }
--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,9 +1,11 @@
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const importCategoryUseCase = new ImportCategoryUseCase();
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
 const importCategoryController = new ImportCategoryController(
   importCategoryUseCase
 );
 
-export { importCategoryController };
+export { importCategoryController, importCategoryUseCase };
