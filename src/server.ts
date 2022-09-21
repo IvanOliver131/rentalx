acab034c39a23c67164b23520272beacf5f210ab
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-09-21 20:17:23 -0300

    finish rentalx

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+// colocar resolveJsonModule como true no tsconfig
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 // Utilizamos isso para aceitar o JSON
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
