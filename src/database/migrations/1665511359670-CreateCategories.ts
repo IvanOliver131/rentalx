70f0b1a537986399ce664b29c20cb3df570a3014
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-10-11 15:03:06 -0300

    feat: migraitons

---
+++ src/database/migrations/1665511359670-CreateCategories.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateCategories1665511359670 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
