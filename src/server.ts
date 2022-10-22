309dbd09de303c62ce1225ef166da8b695290495
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-10-21 21:52:32 -0300

    devCommit

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,6 @@ app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-// app for use routes
 app.use(router);
 
 app.listen(3333, () => console.log("Server is running in port 3333! 🔥🚀"));
