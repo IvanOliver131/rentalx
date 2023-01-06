8067abf1fcac007576e61bc4a6110b86be89c175
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2023-01-06 10:35:09 -0300

    feat: :sparkles: Creating AlterUserAddAvatar
    Creating AlterUserAddAvatar
    
    Creating AlterUserAddAvatar

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,11 @@
+class UpdateUserAvatarUseCase {
+  async execute() {
+    // Adicionar coluna avatar na tabela de users
+    // Refatorar usuário com coluna avatar
+    // Configuração upload multer
+    // Criar regra de negócio do upload
+    // Criar o controller
+  }
+}
+
+export { UpdateUserAvatarUseCase };
